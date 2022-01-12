bc19d2aef94b005f094f79738e93ae4b5120f3e6
NICKO BALUTE <nicksmogu11@gmail.com>
2022-01-13 02:30:19 +0800

    Add Fixes on Scripts

--- js/move.js
+++ js/move.js
@@ -1,17 +1,35 @@
-const circle = document.querySelector(".circle");
+alert("Hello! I am an alert box!!");
 
-switch(e.key) {
-	case 'ArrowLeft':
-		console.log("Pressed Left");
-		break;
-	case 'ArrowRight':
-		console.log("Pressed Left");
-		break;
-	case 'ArrowUp':
-		console.log("Pressed Left");
-		break;
-	case 'ArrowDown':
-		console.log("Pressed Left");
-		break;
-	default: console.log("key not recognise");
+const circle = document.querySelector(".circle");
+let xAxis = 0;
+let yAxis = 0;
+
+function control(e) {
+
+    switch(e.key) {
+        case 'ArrowLeft':
+            console.log("Pressed Left");
+            xAxis -= 50;
+            circle.style.left = xAxis + 'px';
+            break;
+        case 'ArrowRight':
+            console.log("Pressed Right");
+            xAxis += 50;
+            circle.style.left = xAxis + 'px';
+            break;
+        case 'ArrowUp':
+            console.log("Pressed Up");
+            yAxis -= 50;
+            circle.style.top = yAxis + 'px';
+            break;
+        case 'ArrowDown':
+            console.log("Pressed Down");
+            yAxis += 50;
+            circle.style.top = yAxis + 'px';
+            break;
+        default: console.log("key not recognise");
+    }
+
 }
+
+document.addEventListener("keydown", control);
