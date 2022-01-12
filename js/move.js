60c38a1027884f5a18d0563b5b26b9f52c6602d2
NICKO BALUTE <nicksmogu11@gmail.com>
2022-01-13 02:18:45 +0800

    Add Javascript Movement

---
+++ js/move.js
@@ -0,0 +1,17 @@
+const circle = document.querySelector(".circle");
+
+switch(e.key) {
+	case 'ArrowLeft':
+		console.log("Pressed Left");
+		break;
+	case 'ArrowRight':
+		console.log("Pressed Left");
+		break;
+	case 'ArrowUp':
+		console.log("Pressed Left");
+		break;
+	case 'ArrowDown':
+		console.log("Pressed Left");
+		break;
+	default: console.log("key not recognise");
+}
